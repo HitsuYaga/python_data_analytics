963818b847cdbe8126cdbb9a96bc2e341e97929f
HitsuYaga <dq.tho89@gmail.com>
2017-11-19 20:20:58 +0700

    Delete config.py

--- sending-alert-email/config.py
+++
@@ -1,8 +0,0 @@
-MAIL_DEFAULT_SENDER = "<Your email>"
-MAIL_DEFAULT_RECEIVER = "<Client email>"
-SERVER_MAIL_DEFAULT = {
-  "IP": "<IP mail server>",
-  "PORT": "<PORT SMTP>",
-  "USER_LOGON": "<user logon>",
-  "PASSWORD_LOGON": "<password logon>"
-}
