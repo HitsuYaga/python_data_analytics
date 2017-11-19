7fb540ababb835f1e9092e0f63622577978e3bd3
HitsuYaga <dq.tho89@gmail.com>
2017-11-19 20:18:31 +0700

    Update config.py

--- sending-alert-email/config.py
+++ sending-alert-email/config.py
@@ -1,8 +1,8 @@
-MAIL_DEFAULT_SENDER = "duongquangtho@vdc2.com.vn"
-MAIL_DEFAULT_RECEIVER = "demovnptmsslog@vdc2.com.vn"
+MAIL_DEFAULT_SENDER = "<Your email>"
+MAIL_DEFAULT_RECEIVER = "<Client email>"
 SERVER_MAIL_DEFAULT = {
-  "IP": "203.162.141.97",
-  "PORT": "25",
-  "USER_LOGON": "duongquangtho",
-  "PASSWORD_LOGON": "vnpt-vinaphone"
+  "IP": "<IP mail server>",
+  "PORT": "<PORT SMTP>",
+  "USER_LOGON": "<user logon>",
+  "PASSWORD_LOGON": "<password logon>"
 }
